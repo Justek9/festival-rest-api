a6121fc98beb4f9f94e2e12d45c90a0835317e62
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-05 11:53:44 +0200

    init project

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.urlencoded({ extended: false }))
+
+app.listen('8000')
