b719b1b3d4307f1202840f3c461df99c84d4d384
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-06 09:51:32 +0200

    move db to separate file, repair bug with delete endpoint

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const express = require('express')
 const path = require('path')
+const db = require('./db')
 
 const app = express()
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-const db = [
-	{ id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-	{ id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-]
-
 app.get('/testimonials', (req, res) => {
-	res.json(db)
+	res.json(db.testimonials)
 })
 app.get('/testimonials/random', (req, res) => {
-	const random = Math.floor(Math.random() * db.length)
-	res.json(db[random])
+	const random = Math.floor(Math.random() * db.testimonials.length)
+	res.json(db.testimonials[random])
 })
 
 app.get('/testimonials/:id', (req, res) => {
 	const id = Number(req.params.id)
-	const testimonial = db.find(el => el.id === id)
+	const testimonial = db.testimonials.find(el => el.id === id)
 	if (!testimonial) {
 		return res.status(404).json({ message: 'Invalid ID' })
 	}
@@ -29,32 +25,36 @@ app.get('/testimonials/:id', (req, res) => {
 })
 
 app.post('/testimonials', (req, res) => {
-	const id = db[db.length - 1].id + 1
+	const id = db.testimonials[db.testimonials.length - 1].id + 1
 	const newTestimonial = Object.assign({ id: id }, req.body)
-	db.push(newTestimonial)
+	db.testimonials.push(newTestimonial)
 	res.status(201).json({ message: 'OK' })
 })
 
 app.put('/testimonials/:id', (req, res) => {
 	const { author, text } = req.body
 	const id = Number(req.params.id)
-	const testimonial = db.find(el => el.id === id)
-	const index = db.indexOf(testimonial)
+	const testimonial = db.testimonials.find(el => el.id === id)
+	const index = db.testimonials.indexOf(testimonial)
 	if (!testimonial) {
 		return res.status(404).json({ message: 'Invalid ID' })
 	} else {
-		db[index] = { ...testimonial, author, text }
+		db.testimonials[index] = { ...testimonial, author, text }
 		res.json({ message: 'data changed' })
 	}
 })
 
 app.delete('/testimonials/:id', (req, res) => {
 	const id = Number(req.params.id)
-	const testimonial = db.find(el => el.id === id)
+	const testimonial = db.testimonials.find(el => el.id === id)
+	const index = db.testimonials.indexOf(testimonial)
+
 	if (!testimonial) {
 		return res.status(404).json({ message: 'Invalid ID' })
+	} else {
+		db.testimonials.splice(index, 1)
+		res.json({ message: 'OK, deleted' })
 	}
-	res.json({ message: 'OK', data: [...db, req.body] })
 })
 
 app.use((req, res) => {
