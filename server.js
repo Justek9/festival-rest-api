6fa644c1c9e458d099616a53df623ef5ef5bad44
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-10 14:27:36 +0200

    prepare project for publication

--- server.js
+++ server.js
@@ -11,13 +11,21 @@ const app = express()
 app.use(cors())
 
 app.use(express.static(path.join(__dirname, '/public')))
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/api', testimonialRoutes) // add testimonial routes to server
 app.use('/api', concertsRoutes) // add concerts routes to server
 app.use('/api', seatsRoutes) // add seats routes to server
 
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/build/index.html'))
+})
+
 app.use((req, res) => {
 	res.status(404).json({ message: 'Not found...' })
 })
-app.listen('8000')
+app.listen(process.env.PORT || 8000, () => {
+	console.log('Server is running on port: 8000')
+})
